a28231310258c831cc7cb9948b0b68f3879e1b59
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2021-02-15 20:25:12 -0800

    Revert "[dask-on-ray] Add multiple return DataFrame shuffle optimization. (#13951)"
    This reverts commit 99b20d0099894ed08b59e7959f1eac9717cac190.

--- python/ray/util/dask/__init__.py
+++ python/ray/util/dask/__init__.py
@@ -4,16 +4,11 @@ from .callbacks import (
     local_ray_callbacks,
     unpack_ray_callbacks,
 )
-from .optimizations import dataframe_optimize
 
 __all__ = [
-    # Schedulers
     "ray_dask_get",
     "ray_dask_get_sync",
-    # Callbacks
     "RayDaskCallback",
     "local_ray_callbacks",
     "unpack_ray_callbacks",
-    # Optimizations
-    "dataframe_optimize",
 ]
