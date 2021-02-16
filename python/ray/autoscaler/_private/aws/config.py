a651b412fb4c30e50fd08dda689e61fd7eef92ee
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2021-02-15 20:25:12 -0800

    Revert "[AWS] Fill-in AMI if not provided (#13808)"
    This reverts commit 62b9616388a8c84772f9b8612a332ddd3e655a42.

--- python/ray/autoscaler/_private/aws/config.py
+++ python/ray/autoscaler/_private/aws/config.py
@@ -496,13 +496,11 @@ def _check_ami(config):
         # If we do not provide a default AMI for the given region, noop.
         return
 
-    head_ami = config["head_node"].get("ImageId", "").lower()
-    if head_ami in ["", "latest_dlami"]:
+    if config["head_node"].get("ImageId", "").lower() == "latest_dlami":
         config["head_node"]["ImageId"] = default_ami
         _set_config_info(head_ami_src="dlami")
 
-    worker_ami = config["worker_nodes"].get("ImageId", "").lower()
-    if worker_ami in ["", "latest_dlami"]:
+    if config["worker_nodes"].get("ImageId", "").lower() == "latest_dlami":
         config["worker_nodes"]["ImageId"] = default_ami
         _set_config_info(workers_ami_src="dlami")
 
