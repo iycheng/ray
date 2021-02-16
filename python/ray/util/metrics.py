49584cfa631a671cd1c94e93b3514a05800010c9
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2021-02-15 20:25:12 -0800

    Revert "[Metrics] Fix serialization for custom metrics (#13571)"
    This reverts commit 2f8fdce2c7807373d255178d194f8f4b2a995595.

--- python/ray/util/metrics.py
+++ python/ray/util/metrics.py
@@ -147,11 +147,6 @@ class Count(Metric):
         self._metric = CythonCount(self._name, self._description, self._unit,
                                    self._tag_keys)
 
-    def __reduce__(self):
-        deserializer = Count
-        serialized_data = (self._name, self._description, self._tag_keys)
-        return deserializer, serialized_data
-
 
 class Histogram(Metric):
     """Histogram distribution of metric points.
@@ -182,12 +177,6 @@ class Histogram(Metric):
                                        self._unit, self.boundaries,
                                        self._tag_keys)
 
-    def __reduce__(self):
-        deserializer = Histogram
-        serialized_data = (self._name, self._description, self.boundaries,
-                           self._tag_keys)
-        return deserializer, serialized_data
-
     @property
     def info(self):
         """Return information about histogram metric."""
@@ -214,11 +203,6 @@ class Gauge(Metric):
         super().__init__(name, description, tag_keys)
         self._metric = CythonGauge(self._name, self._description, self._unit,
                                    self._tag_keys)
-
-    def __reduce__(self):
-        deserializer = Gauge
-        serialized_data = (self._name, self._description, self._tag_keys)
-        return deserializer, serialized_data
 
 
 __all__ = [
